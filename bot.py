486163ca55346a62c1596792e384968a8b5f7d99
d1pyyallah-dev <d1pyy.allah@gmail.com>
2026-07-28 00:57:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,13 +107,14 @@ async def handle_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.delete()
     await send_codes(chat_id, state["message_id"], phone, context)
 
-def main():
-    asyncio.run(on_startup())
+async def main():
+    await on_startup()
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(spam_callback, pattern="spam"))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_phone))
-    app.run_polling()
+    await app.bot.delete_webhook(drop_pending_updates=True)
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
